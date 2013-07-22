d1e87f7348fe35132209641271cdd5ab8fba99af
iefan <mybsppp@gmail.com>
2013-07-22 17:04:10 +0800

    add one autocommit

---
+++ gitnikola.py
@@ -0,0 +1,29 @@
+import os
+import conf
+import shutil
+
+curdir = os.getcwd()
+outputdir = conf.OUTPUT_FOLDER
+print outputdir
+print curdir
+
+os.chdir(outputdir)
+# shutil.rmtree()
+filelist = [ f for f in os.listdir(".") ]
+for f in filelist:
+    if f == ".git":
+        print 'git should exist! ======================================'
+    else:
+        try:
+            os.remove(f)
+        except:
+            shutil.rmtree(f)
+
+os.chdir(curdir)
+os.system("nikola build")
+os.system("git add .")
+os.system("git status")
+os.system("git pull")
+# os.chdir(outputdir)
+
+# os.system("nikola serve")
